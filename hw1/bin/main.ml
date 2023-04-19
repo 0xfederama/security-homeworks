10c06cd6ff137475f39b5ebd803ec30d3f9ec4e7
Federico <vgfede@gmail.com>
2023-04-20 00:47:12 +0200

    hw1: untrusted/eecute + tests

--- hw1/bin/main.ml
+++ hw1/bin/main.ml
@@ -1,17 +1,148 @@
 open Hw1.Interpreter
 
-let _ = eval (
-    Let("pw", CstS "wrong",
+let execWithFailure test env =
+  let value = 
+    try eval test env
+    with Failure _ ->
+      (* print_endline f; *)
+      Int 1
+  in 
+  assert (value = Int 1)
+
+let execWithoutFailure test env =
+  let value = 
+    try eval test env 
+    with Failure _ -> 
+      (* print_endline f; *)
+      Int 0
+  in 
+  assert (value <> Int 0)
+
+(* Possible cases (in order with the examples below)
+ 1- [ ok ] code executes fun gateway
+ 2- [fail] code accesses secret vars
+ 3- [fail] enclave declares something not secret/gateway
+ 4- [ ok ] include executes fun free
+ 5- [fail] include executes fun enclaved
+ 6- [fail] include access secret var
+ 7- [fail] execute something not untrusted
+ 8- [fail] execute something else (var, ...)
+*)
+let tests = 
+  [
+    (* 1 - code executes gateway fun *)
+    execWithoutFailure (
+        Let("pw", CstS "right",
+            Enclave("encl",
+                Secret("pass", CstS "right",
+                    Secret("check", Fun("arg", Prim("=", Var "arg", Var "pass")),
+                        Gateway("check",
+                            End
+                        )
+                    )
+                ),
+                Let("b", Call(Var "check", Var "pw"), 
+                    If(Var "b", CstSkip, CstSkip)
+                )
+            )
+        )
+    ) [];
+    (* 2 - code accesses secret vars *)
+    execWithFailure (
+        Let("pw", CstS "wrong",
+            Enclave("encl",
+                Secret("pass", CstS "right",
+                    Secret("check", Fun("arg", Prim("=", Var "arg", Var "pass")),
+                        Gateway("check",
+                            End
+                        )
+                    )
+                ),
+                If(Prim("=", Var "pw", Var "pass"), CstSkip, CstSkip)
+            )
+        )
+    ) [];
+    (* 3 - enclave declares something not secret/gateway *)
+    execWithFailure (
         Enclave("encl",
-            Secret("pass", CstS "right",
+            Let("fail", CstSkip, CstSkip),
+            End
+        )
+    ) [];
+    (* 4 - include executes fun free *)
+    execWithoutFailure (
+        Let("f", Fun("arg", CstSkip),
+            Let("untr",
+                IncUntrusted(
+                    Call(Var "f", CstS "toprint")
+                ),
+                Execute("untr", CstSkip)
+            )
+        )
+    ) [];
+    (* 5 - include executes fun enclaved *)
+    execWithFailure (
+        Enclave("enc",
+            Secret("pass", CstS "rightpw",
                 Secret("check", Fun("s", Prim("=", Var "s", Var "pass")),
                     Gateway("check",
                         End
                     )
                 )
             ),
-            Let("b", Call(Var "check", Var "pw"), 
-                If(Var "b", Print(CstS "password ok"), Print(CstS "password differs"))
+            Let("untr",
+                IncUntrusted(
+                    Let("trypw", CstS "rightpw",
+                        Let("b", Call(Var "check", Var "trypw"),
+                            If(Var "b", CstSkip, CstSkip)
+                        )
+                    )
+                ),
+                Execute("untr", CstSkip)
             )
-        ))
-    ) [];;
+        )
+    ) [];
+    (* 6 - include access secret var *)
+    execWithFailure (
+        Enclave("enc",
+            Secret("pass", CstS "rightpw",
+                Secret("check", Fun("s", Prim("=", Var "s", Var "pass")),
+                    Gateway("check",
+                        End
+                    )
+                )
+            ),
+            Let("untr",
+                IncUntrusted(
+                    Let("trypw", CstS "rightpw",
+                        Prim("=", Var "trypw", Var "pass")
+                    )
+                ),
+                Execute("untr", CstSkip)
+            )
+        )
+    ) [];
+    (* 7 - execute something not untrusted *)
+    execWithFailure(
+        Let("f", Fun("arg", CstSkip),
+            Execute("f", CstSkip)
+        )
+    ) [];
+    (* 8 - execute something else (var, ...) *)
+    execWithFailure (
+        Let("f", CstS "no",
+            Execute("f", CstSkip)
+        )
+
+    ) [];
+  ]
+
+let rec execute_tests t i =
+  match t with
+  | [] -> print_endline "Tests passed"
+  | x :: l ->
+      (* print_endline ("Running test case " ^ (string_of_int i)); *)
+      x;
+      execute_tests l (i+1)
+
+let _ = execute_tests tests 0
--- hw1/lib/interpreter.ml
+++ hw1/lib/interpreter.ml
@@ -1,6 +1,7 @@
 open Env
 
 type expr =
+  | CstSkip (* mainly for testing purposes *)
   | CstI of int
   | CstB of bool
   | CstS of string
@@ -15,48 +16,62 @@ type expr =
   | Gateway of ide * expr
   | Print of expr
   | End
+  | IncUntrusted of expr
+  | Execute of ide * expr
+
+type security = Enclaved | Free | Untrusted
 
 type value =
+  | Skip
   | Int of int
   | Bool of bool
   | String of string
-  | Closure of ide * expr * value env
+  | Closure of ide * expr * value env * security
+  | EnclaveClosure of ide
+  | UntrustedClosure of expr
+
+let eval_prim g e1 e2 =
+  match (g, e1, e2) with
+  | "+", Int i1, Int i2 -> Int (i1 + i2)
+  | "-", Int i1, Int i2 -> Int (i1 - i2)
+  | "*", Int i1, Int i2 -> Int (i1 * i2)
+  | "=", Int i1, Int i2 -> Bool (if i1 = i2 then true else false)
+  | ">", Int i1, Int i2 -> Bool (if i1 > i2 then true else false)
+  | "<", Int i1, Int i2 -> Bool (if i1 < i2 then true else false)
+  | "<=", Int i1, Int i2 -> Bool (if i1 <= i2 then true else false)
+  | ">=", Int i1, Int i2 -> Bool (if i1 >= i2 then true else false)
+  | "!=", Int i1, Int i2 -> Bool (if i1 <> i2 then true else false)
+  | "=", String s1, String s2 -> Bool (if s1 = s2 then true else false)
+  | "!=", String s1, String s2 -> Bool (if s1 <> s2 then true else false)
+  | _ -> failwith "unknown primitive or wrong types"
+
+let eval_print x =
+  match x with
+  | Int i ->
+      print_endline (string_of_int i);
+      Int i
+  | Bool b ->
+      print_endline (string_of_bool b);
+      Bool b
+  | String s ->
+      print_endline s;
+      String s
+  | _ -> failwith "can only print int, bool or string"
 
 let rec eval (e : expr) (env : value env) : value =
   match e with
+  | CstSkip -> Skip
   | CstI i -> Int i
   | CstB b -> Bool b
   | CstS s -> String s
   | Var x -> lookup env x
-  | Print x -> (
+  | Print x ->
       let xval = eval x env in
-      match xval with
-      | Int i ->
-          print_endline (string_of_int i);
-          Int i
-      | Bool b ->
-          print_endline (string_of_bool b);
-          Bool b
-      | String s ->
-          print_endline s;
-          String s
-      | _ -> failwith "can only print int, bool or string")
-  | Prim (op, e1, e2) -> (
+      eval_print xval
+  | Prim (op, e1, e2) ->
       let v1 = eval e1 env in
       let v2 = eval e2 env in
-      match (op, v1, v2) with
-      | "+", Int i1, Int i2 -> Int (i1 + i2)
-      | "-", Int i1, Int i2 -> Int (i1 - i2)
-      | "*", Int i1, Int i2 -> Int (i1 * i2)
-      | "=", Int i1, Int i2 -> Bool (if i1 = i2 then true else false)
-      | "=", String s1, String s2 -> Bool (if s1 = s2 then true else false)
-      | ">", Int i1, Int i2 -> Bool (if i1 > i2 then true else false)
-      | "<", Int i1, Int i2 -> Bool (if i1 < i2 then true else false)
-      | "<=", Int i1, Int i2 -> Bool (if i1 <= i2 then true else false)
-      | ">=", Int i1, Int i2 -> Bool (if i1 >= i2 then true else false)
-      | "!=", Int i1, Int i2 -> Bool (if i1 <> i2 then true else false)
-      | "!=", String s1, String s2 -> Bool (if s1 <> s2 then true else false)
-      | _ -> failwith "unknown primitive or wrong type")
+      eval_prim op v1 v2
   | If (e1, e2, e3) -> (
       match eval e1 env with
       | Bool true -> eval e2 env
@@ -68,36 +83,101 @@ let rec eval (e : expr) (env : value env) : value =
         let xval = eval rhs env in
         let letenv = (x, xval) :: env in
         eval body letenv
-  | Fun (x, body) -> Closure (x, body, env)
+  | Fun (x, body) -> Closure (x, body, env, Free)
   | Call (eFun, eArg) -> (
       let fClosure = eval eFun env in
       match fClosure with
-      | Closure (x, body, decenv) ->
-          let xval = eval eArg env in
-          let bodyenv = (x, xval) :: decenv in
-          eval body bodyenv
+      | Closure (x, body, decenv, sec) ->
+          if sec = Untrusted then
+            failwith "untrusted code must be executed inside \"execute\""
+          else
+            let xval = eval eArg env in
+            let bodyenv = (x, xval) :: decenv in
+            eval body bodyenv
       | _ -> failwith "call not on a function")
   | Enclave (x, rhs, body) ->
-      if search env x then failwith (x ^ " already defined")
-      else
-        let rec eval_enc r glob_env enc_env =
-          match r with
-          | Fun (x, body) -> Closure (x, body, enc_env)
-          | Secret (id, rhs, b) ->
-              if search env x then failwith (id ^ " already defined")
-              else
-                let idval = eval rhs enc_env in
-                let enc_env = (id, idval) :: enc_env in
-                eval_enc b glob_env enc_env
-          | Gateway (id, b) ->
-              let idval = lookup enc_env id in
-              let glob_env = (id, idval) :: env in
+      let rec eval_enc r glob_env enc_env =
+        match r with
+        | Secret (id, rhs, b) ->
+            if search env id then failwith (id ^ " already defined")
+            else
+              let idval = eval rhs enc_env in
+              let idvalenc =
+                match idval with
+                | Closure (a1, b1, c1, _) -> Closure (a1, b1, c1, Enclaved)
+                | _ -> idval
+              in
+              let enc_env = (id, idvalenc) :: enc_env in
               eval_enc b glob_env enc_env
-          | End -> eval body glob_env
-          | _ -> failwith "not a fun, secret, gateway or end"
-        in
-        eval_enc rhs env env
+        | Gateway (id, b) ->
+            let idval = lookup enc_env id in
+            let glob_env = (id, idval) :: env in
+            eval_enc b glob_env enc_env
+        | CstSkip -> Skip
+        | End ->
+            let newenv = (x, EnclaveClosure x) :: glob_env in
+            eval body newenv
+        | _ -> failwith "not a fun, secret, gateway or end"
+      in
+      eval_enc rhs env env
   | Secret (_, _, _) -> failwith "secrets can only be defined inside enclaves"
   | Gateway (_, _) -> failwith "gateways can be only defined inside enclaves"
-  | End -> failwith "end can only end an enclave"
-(* | _ -> failwith "eval error" *)
+  | End -> failwith "end can only end an enclave or an include"
+  | IncUntrusted rhs -> UntrustedClosure rhs
+  | Execute (x, body) ->
+      let xval = lookup env x in
+      let newenv = env in
+      let _ =
+        match xval with
+        | UntrustedClosure rhs ->
+            let rec eval_untr r uenv =
+              match r with
+              | CstSkip -> Skip
+              | CstI i -> Int i
+              | CstB b -> Bool b
+              | CstS s -> String s
+              | Var x -> lookup uenv x
+              | Print x ->
+                  let xval = eval_untr x uenv in
+                  eval_print xval
+              | Prim (op, e1, e2) ->
+                  let v1 = eval_untr e1 uenv in
+                  let v2 = eval_untr e2 uenv in
+                  eval_prim op v1 v2
+              | If (e1, e2, e3) -> (
+                  match eval_untr e1 uenv with
+                  | Bool true -> eval_untr e2 uenv
+                  | Bool false -> eval_untr e3 uenv
+                  | _ -> failwith "not a boolean guard")
+              | Fun (y, b) -> Closure (y, b, env, Untrusted)
+              | Call (eFun, eArg) -> (
+                  let fClosure = eval_untr eFun uenv in
+                  match fClosure with
+                  | Closure (x, body, decenv, sec) ->
+                      if sec = Enclaved then
+                        failwith
+                          "can't call enclaved functions in untrusted code"
+                      else
+                        let xval = eval_untr eArg uenv in
+                        let bodyenv = (x, xval) :: decenv in
+                        eval_untr body bodyenv
+                  | _ -> failwith "call not on a function")
+              | Let (y, rh, bod) ->
+                  if search uenv y then failwith (y ^ " already defined")
+                  else
+                    let yval = eval_untr rh uenv in
+                    let uenv = (y, yval) :: uenv in
+                    eval_untr bod uenv
+              | Enclave _ -> failwith "can't use enclave inside include"
+              | End -> failwith "should not need end"
+              | Secret (_, _, _) -> failwith "can't use secret inside untrusted"
+              | Gateway (_, _) -> failwith "can't use gateway inside untrusted"
+              | IncUntrusted _ ->
+                  failwith "can't include untrusted code inside untrusted"
+              | Execute _ ->
+                  failwith "can't execute untrusted code inside untrusted"
+            in
+            eval_untr rhs newenv
+        | _ -> failwith (x ^ " is not untrusted code")
+      in
+      eval body newenv
